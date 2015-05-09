d2d0a8a4f911d6a46cf4def218dabc9ca2c50ed4
Dan Asselin <dasselin86@gmail.com>
2015-05-09 17:42:37 -0400

    Create Recipe class to wrap desired JSON data from 2nd API call
    All attributes for Recipe object, created on invocation of BigOvenGetRecipeJson function, are functional except for ingredients, which requires creation of new JS constructors.

--- workshop/www/js/recipe.js
+++ workshop/www/js/recipe.js
@@ -1,1 +1,29 @@
-recipe.js
+var Recipe = function(data){
+  this.title = data.Title;
+  this.ingredients = [];
+  this.instructions = data.Instructions;
+  this.prepTime = data.TotalMinutes;
+  this.yieldNumber = data.YieldNumber;
+  this.yieldUnit = data.YieldUnit;
+  this.stars = data.StarRating;
+  this.instructions = data.Instructions;
+  this.imageUrl = data.ImageURL
+}
+
+function BigOvenGetRecipeJson(recipeId) {
+  var apiKey = APIKEY;
+  var url = "https://api.bigoven.com/recipe/" + recipeId + "?api_key="+apiKey;
+
+  $.ajax({
+    type: "GET",
+    dataType: 'json',
+    cache: false,
+    url: url
+  }).then(function(data) {
+    var currentRecipe = new Recipe(data);
+  });
+};
+
+$(document).ready(function(){
+  BigOvenGetRecipeJson(100);
+});
