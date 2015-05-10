4bcf3fa5f4b326a93482c396783e68c13140a857
Dan Asselin <dasselin86@gmail.com>
2015-05-10 17:19:01 -0400

    Add code to favorite.rb to POST new favorite to server
    Using iOS emulator, user can create new favorites back on Sinatra server. Conditional with #to_json branches untested as of yet.

--- simple_backend/app/controllers/favorite.rb
+++ simple_backend/app/controllers/favorite.rb
@@ -1,9 +1,17 @@
 post '/favorites' do
-  # could create a parser class to do the below JSON-parsing actions
-  # big_oven_id = parsed json request for big_oven_id number
-  # user_name = parsed json request for user_name
-  # recipe = find or create a recipe by searching DB for big_oven_id
-  # user = find user by user_name variable
-  # favorite = create new favorite with recipe and user data
-  # redirect to user's dashboard with new favorite object
+  big_oven_id = params[:bigOvenId].to_s
+  user_name = params[:userName]
+  recipe = Recipe.find_or_create_by(big_oven_id: big_oven_id)
+  user = User.find_or_create_by(name: user_name)
+
+  favorite = Favorite.new( user_id:   user.id,
+                           recipe_id: recipe.id
+                         )
+
+  if favorite.save
+    return { status: "success" }.to_json
+  else
+    return { status: "fail" }.to_json
+  end
+
 end
