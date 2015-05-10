4c402adcaa00ce586a7a75e10486ce0eed9bb458
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-10 13:20:29 -0400

    post routes for auth controller

--- simple_backend/app/controllers/auth.rb
+++ simple_backend/app/controllers/auth.rb
@@ -1,11 +1,23 @@
-get '/' do
-  erb :welcome
+post '/signin' do
+  content_type :json
+  user = User.find_by(name: params[:user][:name])
+  if user && user.authenticate(params[:user][:password])
+    session[:user_id] = user.id
+    return { status: "success", id: user.id, name: user.name }.to_json
+  else
+    return { status: "failed" }
+  end
 end
 
-get '/login' do
-  
+post '/signup' do
+  user = User.create(params[:user])
+  session[:user_id] = user.id
+  content_type :json
+  return { status: "success", id: user.id, name: user.name }.to_json
 end
 
-get '/signup' do
-  
+get '/signout' do
+  session[:user_id] = nil
+  content_type :json
+  return { status: "success" }
 end
