1d9d6ad391d8348670c95706faf498de77be61ab
Dan Asselin <dasselin86@gmail.com>
2015-05-10 15:51:04 -0400

    Psuedocode POST favorites route

---
+++ simple_backend/app/controllers/favorite.rb
@@ -0,0 +1,9 @@
+post '/favorites' do
+  # could create a parser class to do the below JSON-parsing actions
+  # big_oven_id = parsed json request for big_oven_id number
+  # user_name = parsed json request for user_name
+  # recipe = find or create a recipe by searching DB for big_oven_id
+  # user = find user by user_name variable
+  # favorite = create new favorite with recipe and user data
+  # redirect to user's dashboard with new favorite object
+end
