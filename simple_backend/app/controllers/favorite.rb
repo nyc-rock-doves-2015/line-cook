2634ed4931197848435faa473df31bf3d1f8c127
Dan Asselin <dasselin86@gmail.com>
2015-05-11 14:45:18 -0400

    Write conditional to handle unlogged-in users who try to favorite a recipe

--- simple_backend/app/controllers/favorite.rb
+++ simple_backend/app/controllers/favorite.rb
@@ -1,19 +1,24 @@
 post '/favorites' do
   content_type :json
-
-  big_oven_id = params[:bigOvenId].to_s
-  user_name = params[:userName]
-  recipe = Recipe.find_or_create_by(big_oven_id: big_oven_id)
-  user = User.find_or_create_by(name: user_name)
-
-  favorite = Favorite.new( user_id:   user.id,
-                           recipe_id: recipe.id
-                         )
-
-  if favorite.save
-    return { status: "success" }.to_json
+  if session[:user_id]
+    user = User.find(session[:user_id])
   else
-    return { status: "fail" }.to_json
+    return { alert: "Must be logged in to choose favorites." }.to_json
   end
+
+  recipe = Recipe.find_or_create_by(title: params[:title],
+                                    big_oven_id: params[:bigOvenId].to_i
+                                    )
+
+  p recipe
+  # favorite = Favorite.new( user_id:   user.id,
+  #                          recipe_id: recipe.id
+  #                        )
+
+  # if favorite.save
+  #   return { status: "success" }.to_json
+  # else
+  #   return { status: "fail" }.to_json
+  # end
 
 end
