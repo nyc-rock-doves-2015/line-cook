84770dbfa4da78bbf8cf6f6520541db5f72cd3b3
Dan Asselin <dasselin86@gmail.com>
2015-05-11 15:50:47 -0400

    Delete unneeded /signout route in Sinatra

--- simple_backend/app/controllers/auth.rb
+++ simple_backend/app/controllers/auth.rb
@@ -14,12 +14,6 @@ post '/signup' do
   puts "made it to signup"
   content_type :json
   user = User.create(params[:user])
-  session[:user_id] = user.id
   return { status: "success", id: user.id, name: user.name }.to_json
 end
 
-get '/signout' do
-  session[:user_id] = nil
-  content_type :json
-  return { status: "success" }
-end
