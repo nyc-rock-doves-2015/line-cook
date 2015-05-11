8db8cea91b279a9d02cb40728c110dc25d6a3327
Dan Asselin <dasselin86@gmail.com>
2015-05-11 15:50:17 -0400

    Add #to_json method to fail branch of conditional in sign-in route

--- simple_backend/app/controllers/auth.rb
+++ simple_backend/app/controllers/auth.rb
@@ -2,13 +2,11 @@ post '/signin' do
   content_type :json
   user = User.find_by(name: params[:user][:name])
   puts "found user"
-  p user
   if user && user.authenticate(params[:user][:password])
-    session[:user_id] = user.id
     puts "Successfully signed in!!!!"
     return { status: "success", id: user.id, name: user.name }.to_json
   else
-    return { status: "failed" }
+    return { status: "failed" }.to_json
   end
 end
 
