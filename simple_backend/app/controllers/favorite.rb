ba0f79a584a78e9397c5738e0844a3cc36783cb1
Dan Asselin <dasselin86@gmail.com>
2015-05-11 17:41:45 -0400

    Working POST /favorites route in Sinatra

--- simple_backend/app/controllers/favorite.rb
+++ simple_backend/app/controllers/favorite.rb
@@ -1,24 +1,18 @@
 post '/favorites' do
   content_type :json
-  if session[:user_id]
-    user = User.find(session[:user_id])
-  else
-    return { alert: "Must be logged in to choose favorites." }.to_json
-  end
-
   recipe = Recipe.find_or_create_by(title: params[:title],
                                     big_oven_id: params[:bigOvenId].to_i
                                     )
+  user = User.find(params[:userId])
 
-  p recipe
-  # favorite = Favorite.new( user_id:   user.id,
-  #                          recipe_id: recipe.id
-  #                        )
+  favorite = Favorite.new( user_id:   user.id,
+                           recipe_id: recipe.id
+                         )
 
-  # if favorite.save
-  #   return { status: "success" }.to_json
-  # else
-  #   return { status: "fail" }.to_json
-  # end
+  if favorite.save
+    return { status: "Favorited!" }.to_json
+  else
+    return { status: "Sorry, something went wrong." }.to_json
+  end
 
 end
