f14b1622b3146958eebc81715b1f6e16557ba642
Dan Asselin <dasselin86@gmail.com>
2015-05-11 21:12:15 -0400

    Add #favorite_recipes model method to user model in simple_backend

--- simple_backend/app/models/user.rb
+++ simple_backend/app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
 
   has_many :favorites
   has_many :recipes, through: :favorites
+
+  def favorite_recipes
+    self.favorites.map do |favorite|
+      {:title => favorite.recipe.title,
+       :bigOvenId => favorite.recipe.big_oven_id
+      }
+    end
+  end
 end
