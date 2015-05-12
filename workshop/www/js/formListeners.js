d35182eb27e96d189b70ccb98b20f4f53b54431c
Dan Asselin <dasselin86@gmail.com>
2015-05-12 18:04:36 -0400

    Add formListeners.js with recipeSearchEvent function

---
+++ workshop/www/js/formListeners.js
@@ -0,0 +1,10 @@
+var recipeSearchEvent = function(apiRequest){
+  $('.container').on('submit', '#search-form', function(event) {
+    event.preventDefault();
+    var data = $('#search').val();
+    $('#search').val('');
+    renderSplash('#logged-in', '#logged-out', '.container')
+    $('body').css("background-color", "#FFF")
+    apiRequest(data);
+  });
+}
