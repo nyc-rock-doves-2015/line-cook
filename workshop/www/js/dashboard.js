405c62071be0cdd28f241a2472033e4b6414262b
Dan Asselin <dasselin86@gmail.com>
2015-05-12 11:05:00 -0400

    Pass user view to Mustache template
    In dashboard.js, pass user attribute from response into output variable to be rendered in handlebars for the user dashboard template in index.html.

--- workshop/www/js/dashboard.js
+++ workshop/www/js/dashboard.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
       data: { userId: userId }
     }).then(function(response){
       var template = $('#user-dashboard-template').html();
-      var output = Mustache.render(template, { favorites: response.dashboardInfo });
+      var output = Mustache.render(template, { favorites: response.dashboardInfo, name: response.userName });
     $('.content-container').html(output);
     });
   });
