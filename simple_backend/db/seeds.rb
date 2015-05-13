c94c7df0ba155a01e11bbb0fd226a660eed262e6
suprfrye <tara.c.frye@gmail.com>
2015-05-13 11:15:52 -0400

    User Big Oven recipe info to create recipe objects

--- simple_backend/db/seeds.rb
+++ simple_backend/db/seeds.rb
@@ -7,3 +7,11 @@ recipes = []
 10.times do
   users << User.create(name: Faker::Name.first_name, password: password)
 end
+
+recipes << Recipe.create(big_oven_id: 161255, title: "Shrimp and Broccoli")
+recipes << Recipe.create(big_oven_id: 163948, title: "Salmon Burgers")
+recipes << Recipe.create(big_oven_id: 630008, title: "Lasagna")
+recipes << Recipe.create(big_oven_id: 196149, title: "Lasagna")
+recipes << Recipe.create(big_oven_id: 381400, title: "Pizza")
+recipes << Recipe.create(big_oven_id: 337784, title: "Cowboy Cookies")
+recipes << Recipe.create(big_oven_id: 230679, title: "Sugar Cookies")
