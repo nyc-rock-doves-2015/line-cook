1271d9e5f83b9d11a5107db24f02de8458ef6f09
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-13 19:24:38 -0400

    extract openears setup into separate js file

--- workshop/www/js/big-oven-recipe.js
+++ workshop/www/js/big-oven-recipe.js
@@ -25,3 +25,85 @@ var renderSingleRecipe = function(data) {
   
   return instructions;
 }
+
+var openEarsSetup = function(data) {
+
+  $('.content-container').on('click', '#cook-button', function(event) {
+
+    renderPage('#recipe-back-template', '.content-container')
+    renderAppend('#instructions-template', '.content-container', {instructions: currentRecipe.instructions})
+    renderAppend('#backup-buttons-template', '.content-container')
+
+    window.scrollTo(0, 0);
+
+    var instructions = data;
+    var instructionsIndex = 0;
+
+    Ears.stopListening();
+    Ears.resumeListening();
+
+    $('.content-container').on('click', '.backup-start', function(event) {
+      Ears.stopListening();
+      Ears.resumeListening();
+      instructionsIndex = 0;
+      Ears.say(instructions[instructionsIndex]);
+      instructionsIndex += 1;
+    })
+
+    $('.content-container').on('click', '.backup-next', function(event) {
+      Ears.say(instructions[instructionsIndex]);
+      instructionsIndex += 1;
+    })
+
+    $('.content-container').on('click', '.backup-repeat', function(event) {
+      instructionsIndex -= 1;
+      Ears.say(instructions[instructionsIndex]);
+      instructionsIndex += 1;
+    })
+
+    $('.content-container').on('click', '.backup-off', function(event) {
+      Ears.say("Why don't you love me?")
+      Ears.stopListening();
+    })
+
+    var languages = {};
+    languages["commands"] = {};
+    languages["commands"].name = "commands";
+    languages["commands"].csv = "START,NEXT,REPEAT,QUIT";
+    languages["commands"].paths = {};
+    Ears.generateLanguageModel(languages["commands"].name, languages["commands"].csv);
+    $(document).on("generateLanguageModel", function(evt) {
+      languages["commands"].paths = evt.originalEvent.detail;
+    });
+
+    var processHeard = function(detail) {
+      if (detail.hypothesis == "NEXT") {
+        Ears.say(instructions[instructionsIndex]);
+        instructionsIndex += 1;
+      } else if (detail.hypothesis == "START") {
+        Ears.stopListening();
+        Ears.resumeListening();
+        instructionsIndex = 0;
+        Ears.say(instructions[instructionsIndex]);
+        instructionsIndex += 1;
+      } else if (detail.hypothesis == "REPEAT") {
+        instructionsIndex -= 1;
+        Ears.say(instructions[instructionsIndex]);
+        instructionsIndex += 1;
+      } else if (detail.hypothesis == "QUIT") {
+        Ears.say("Why don't you love me?")
+        Ears.stopListening();
+      }
+    };
+
+    $(document).on("receivedHypothesis", function(evt) {
+      detail = evt.originalEvent.detail;
+      processHeard(detail);
+    });
+
+    $(document).on("finishedSpeaking", function(evt) {
+      if (instructionsIndex >= instructions.length) { Ears.stopListening(); }
+    });
+
+  });
+}
