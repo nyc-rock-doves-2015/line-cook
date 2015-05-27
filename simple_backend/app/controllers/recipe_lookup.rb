ce7fcc59c475c338cdb6b25a7b23cb30b0132640
Ben Morimoto <bkmorimoto@gmail.com>
2015-05-27 17:41:27 -0400

    add url encoding for title keyword coming from the phone

--- simple_backend/app/controllers/recipe_lookup.rb
+++ simple_backend/app/controllers/recipe_lookup.rb
@@ -1,3 +1,4 @@
 get '/search_recipes' do
-  RestClient.get "https://api.bigoven.com/recipes?pg=1&rpp=25&title_kw=#{params[:title_keyword]}&api_key=#{ENV['API_KEY']}",{:Accept => 'application/json'}
+  title_keyword = URI.escape(params[:title_keyword])
+  RestClient.get "https://api.bigoven.com/recipes?pg=1&rpp=25&title_kw=#{title_keyword}&api_key=#{ENV['API_KEY']}",{:Accept => 'application/json'}
 end
